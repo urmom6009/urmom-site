ffceb324025ab0f50fc47de9e7290dba5f41bba0
urmom6009 <cvarg3695@gmail.com>
2026-05-11 00:03:51 -0700

    attempt to solve stinky cloudflare from whining

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.urmom.systems",
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: cloudflare(),
 });
