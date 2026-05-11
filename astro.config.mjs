333d83f2e8d9e939eb7102da53a8fcbc1c5c0e50
urmom6009 <cvarg3695@gmail.com>
2026-05-10 23:56:12 -0700

    load tailwind styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.urmom.systems",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare()
 });
